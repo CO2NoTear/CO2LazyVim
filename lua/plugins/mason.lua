de6497183c7a8d8650051c39e2fe31dd353885cd
CO2NoTear <sqede@outlook.com>
2025-05-14 03:08:45 +0800

    feat: deprecated mirror; add neovide

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -14,8 +14,8 @@ return {
       -- 2. The release version (e.g. "v0.3.0")
       -- 3. The asset name (e.g. "rust-analyzer-v0.3.0-x86_64-unknown-linux-gnu.tar.gz")
       --NOTE: default is this:
-      -- download_url_template = "https://github.com/%s/releases/download/%s/%s",
-      download_url_template = "https://mirror.ghproxy.com/https://github.com/%s/releases/download/%s/%s",
+      download_url_template = "https://github.com/%s/releases/download/%s/%s",
+      -- download_url_template = "https://mirror.ghproxy.com/https://github.com/%s/releases/download/%s/%s",
     },
   },
 }
