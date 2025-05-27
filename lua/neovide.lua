47b999733075b74bf463bc85d1c309e865391fc1
CO2NoTear <sqede@outlook.com>
2025-05-28 02:29:38 +0800

    feat: neovide透明度

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -1,4 +1,10 @@
 if vim.g.neovide then
   -- Put anything you want to happen only in Neovide here
   vim.g.neovide_input_macos_option_key_is_meta = "only_left"
+
+  vim.g.neovide_opacity = 0.8
+  vim.g.neovide_normal_opacity = 0.7
+  vim.g.neovide_window_blurred = true
+
+  vim.g.neovide_underline_stroke_scale = 1.5
 end
