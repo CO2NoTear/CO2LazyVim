8014c171e09491dc242370d5410234a041195148
CO2NoTear <sqede@outlook.com>
2025-06-16 17:34:01 +0800

    feat: neovide remote detach（貌似没用）/ 新增Oil插件

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -1,6 +1,7 @@
 if vim.g.neovide then
   -- Put anything you want to happen only in Neovide here
   vim.g.neovide_input_macos_option_key_is_meta = "only_left"
+  vim.g.neovide_detach_on_quit = "always_detach"
 
   vim.g.neovide_opacity = 0.8
   vim.g.neovide_normal_opacity = 0.7
