c8317dfe53a178b59318315fb778c6f37539260b
CO2NoTear <sqede@outlook.com>
2025-06-17 16:12:44 +0800

    feat: gitmirror for mason

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -14,8 +14,10 @@ return {
       -- 2. The release version (e.g. "v0.3.0")
       -- 3. The asset name (e.g. "rust-analyzer-v0.3.0-x86_64-unknown-linux-gnu.tar.gz")
       --NOTE: default is this:
-      download_url_template = "https://github.com/%s/releases/download/%s/%s",
+      download_url_template = "https://hub.gitmirror.com/https://github.com/%s/releases/download/%s/%s",
       -- download_url_template = "https://mirror.ghproxy.com/https://github.com/%s/releases/download/%s/%s",
     },
+    { "mason-org/mason.nvim", version = "^1.0.0" },
+    { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
   },
 }
