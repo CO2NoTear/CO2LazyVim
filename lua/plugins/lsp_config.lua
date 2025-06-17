9c88564d19991271358c11a85be588f373f58a3c
CO2NoTear <sqede@outlook.com>
2025-06-17 20:35:01 +0800

    fix: clangd config fix

--- lua/plugins/lsp_config.lua
+++ lua/plugins/lsp_config.lua
@@ -1,24 +1,3 @@
 return {
   "neovim/nvim-lspconfig",
-  opts = {
-    setup = {
-      clangd = function(_, opts)
-        opts.cmd = {
-          "clangd",
-          "--background-index",
-          "--clang-tidy",
-          "--header-insertion=iwyu",
-          "--completion-style=detailed",
-          "--function-arg-placeholders",
-          "--fallback-style=llvm",
-        }
-        opts.init_options = {
-          usePlaceholders = true,
-          completeUnimported = true,
-          clangdFileStatus = true,
-          fallbackFlags = { "-std=c++20" },
-        }
-      end,
-    },
-  },
 }
--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -5,6 +5,24 @@ return {
       neocmake = {
         enabled = false,
       },
+      clangd = {
+        cmd = {
+          "clangd",
+          "--background-index",
+          "--clang-tidy",
+          "--header-insertion=iwyu",
+          "--completion-style=detailed",
+          "--function-arg-placeholders",
+          "--fallback-style=llvm",
+        },
+        init_options = {
+          usePlaceholders = true,
+          completeUnimported = true,
+          clangdFileStatus = true,
+          fallbackFlags = { "-std=c++20" },
+        },
+        filetypes = { "c", "cpp", "objc", "objcpp", "cuda" }, -- Exclude "proto".
+      },
     },
   },
 }
